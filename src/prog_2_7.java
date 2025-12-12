606d02b0041587fb779cabe0a310758bfda8c1de
tiyasa2005 <24051443@kiit.ac.in>
2025-12-12 23:44:22 +0530

    update

---
+++ src/prog_2_7.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Rectangle2{
+    int length;
+    int breadth;
+    Rectangle2() {
+        length = 0;
+        breadth = 0;
+    }
+    Rectangle2(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+    int area() {
+        return length * breadth;
+    }
+}
+
+public class prog_2_7 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Rectangle2 box = new Rectangle2();
+        System.out.println("Area using default constructor: " + box.area());
+        System.out.print("Enter length: ");
+        int length = sc.nextInt();
+        System.out.print("Enter breadth: ");
+        int breadth = sc.nextInt();
+        Rectangle2 box2 = new Rectangle2(length, breadth);
+        System.out.println("Area using parameterized constructor: " + box2.area());
+    }
+}
