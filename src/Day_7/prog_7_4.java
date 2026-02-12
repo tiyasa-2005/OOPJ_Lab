173dff9101ff3d7aff81c6c1b8f6f60714775d8f
tiyasa2005 <24051443@kiit.ac.in>
2026-02-12 23:44:21 +0530

    update

--- src/Day_7/prog_7_4.java
+++ src/Day_7/prog_7_4.java
@@ -1,73 +1,63 @@
 package Day_7;
 
-import java.util.*;
+import java.util.Scanner;
 
 class HrsException extends Exception {
-    public HrsException(String message) {
-        super(message);
-    }
 }
 
 class MinException extends Exception {
-    public MinException(String message) {
-        super(message);
-    }
 }
 
-//class SecException extends Exception {
-//    public SecException(String message) {
-//        super(message);
-//    }
-//}
-//
-//class Time {
-//    int hrs, mins, sec;
-//
-//
-//    void input() {
-//        try {
-//            Scanner sc = new Scanner(System.in);
-//            System.out.println("Enter hours, minutes and seconds:");
-//            hrs = sc.nextInt();
-//            mins = sc.nextByte();
-//            sec = sc.nextInt();
-//        }
-//        if (hrs > 24 && hrs < 0) {
-//            throw new HrsException("Hours should be more than 0 and less than 24");
-//        }
-//        if (mins > 60 && mins < 0) {
-//            throw new MinException("Minutes should be more than 0 and less than 60");
-//        }
-//        if (sec > 60 && sec < 0) {
-//            throw new SecException("Seconds should be more than 0 and less than 60");
-//        }
-//
-//    catch(HrsException e1){
-//            System.out.println("Exception occurred: " + e1);
-//        }
-//                MinException e2)
-//        SecException e3
-//
-//
-//                {
-//
-//    })
-//
-//        {
-//            System.out.println("Exception occurred: " + e2);
-//        }
-//    catch(
-//
-//                {
-//                        System.out.println("Exception occurred: " + e3);
-//    })
-//    }
-//}
-//}
-//
-//public class prog_7_4 {
-//    public static void main(String[] args) {
-//        Time ob = new Time();
-//        ob.input();
-//    }
-//}
+class SecException extends Exception {
+}
+
+class Time {
+    int hrs, mins, sec;
+
+    void input() {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter hours, minutes and seconds:");
+            hrs = sc.nextInt();
+            mins = sc.nextInt();
+            sec = sc.nextInt();
+            if (hrs > 24 || hrs < 0 || mins > 60 || mins < 0 || sec > 60 || sec < 0) {
+                System.out.println("Caught the exception");
+            }
+            else {
+                System.out.println("Correct Time-> " + hrs + ":" + mins + ":" + sec);
+                return;
+            }
+
+            try {
+                if (hrs > 24 || hrs < 0)
+                    throw new HrsException();
+            }
+            catch (HrsException e) {
+                System.out.println("Exception occurred: InvalidHourException:hour is not greater than 24");
+            }
+
+            try {
+                if (mins > 60 || mins < 0)
+                    throw new MinException();
+            }
+            catch (MinException e) {
+                System.out.println("Exception occurred: InvalidMinuteException:hour is not greater than 60");
+            }
+
+            // Validate Seconds
+            try {
+                if (sec > 60 || sec < 0)
+                    throw new SecException();
+            }
+            catch (SecException e) {
+                System.out.println("Exception occurred: InvalidSecondException:hour is not greater than 60");
+            }
+        }
+    }
+
+class prog_7_4 {
+    public static void main(String[] args) {
+        Time ob = new Time();
+        ob.input();
+    }
+}
